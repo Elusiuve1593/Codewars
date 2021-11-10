0e0a457f229d3822a92b1c777cd92e13a9ca0281
Elusiuve1593 <elusiuve15.09.93@icloud.com>
2021-11-10 19:23:30 +0200

    Create 21.js

---
+++ 21.js
@@ -0,0 +1,30 @@
+function getPlanetName(id) {
+  var name;
+  switch (id) {
+    case 1:
+      return (name = "Mercury");
+      break;
+    case 2:
+      return (name = "Venus");
+      break;
+    case 3:
+      return (name = "Earth");
+      break;
+    case 4:
+      return (name = "Mars");
+      break;
+    case 5:
+      return (name = "Jupiter");
+      break;
+    case 6:
+      return (name = "Saturn");
+      break;
+    case 7:
+      return (name = "Uranus");
+      break;
+    case 8:
+      return (name = "Neptune");
+      break;
+  }
+  return name;
+}
