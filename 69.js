23158d5a6fd91feec5f1613967bae749f0544ec9
Elusiuve1593 <elusiuve15.09.93@icloud.com>
2021-11-16 18:27:06 +0200

    Create 69.js

---
+++ 69.js
@@ -0,0 +1,9 @@
+function mergeArrays(arr1, arr2) {
+  return [...arr1, ...arr2]
+    .sort((a, b) => {
+      return a - b;
+    })
+    .filter((el, index, arr) => {
+      return arr.indexOf(el) === index;
+    });
+}
